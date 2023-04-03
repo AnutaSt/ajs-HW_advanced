790f53a2d0a7f11276d2b85f6925aadc95d8a68d
AnnaSt <anutabatuk@gmail.com>
2023-04-03 14:52:08 +0800

    sorting

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -1,12 +1,20 @@
 export default function sorting(obj, array) {
   const result = [];
   const copyObj = { ...obj };
+  
+  array.forEach((elem => {
+    result.push({ key: elem, value: copyObj[elem] });
+    delete copyObj[elem];
+  };
 
-  array.forEach((elem) => {
-    if (elem in copyObj) {
-      result.push({ key: elem, value: copyObj[elem] });
-      delete copyObj[elem];
-    }
+  const temp = [];
+  for(const key in copyObj) {
+    temp.push({ key: key, value: copyObj[key]});
+  }
+
+  temp.sort((a, b) => {
+    if (a.key < b.key) return -1;
+    if (a.key > b.key) return 1;
   });
 
   const arrKeys = Object.keys(copyObj).sort();
